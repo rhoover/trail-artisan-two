e7d196df6382c080d4b308dbf96456ed4750e9a8
rhoover <robin.hoover@gmail.com>
2025-04-05 09:40:08 -0400

    Better Back Button

--- src/js/inline/backbutton.js
+++ src/js/inline/backbutton.js
@@ -3,6 +3,11 @@
 
   var backButton = document.querySelector('.back-button');
 
+  // history.length is always 1 or 2 when landing on site for first time
+  if (history.length > 2) {
+    backButton.classList.add('back-button-active');
+  };
+
   backButton.onclick = function () {
     history.back();
   };
