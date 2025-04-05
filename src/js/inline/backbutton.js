ed3c49c83a292f907ba27fb7734b0b030abffe6c
rhoover <robin.hoover@gmail.com>
2025-04-05 11:07:02 -0400

    Improved And Working

--- src/js/inline/backbutton.js
+++ src/js/inline/backbutton.js
@@ -4,7 +4,14 @@
   var backButton = document.querySelector('.back-button');
 
   // history.length is always 1 or 2 when landing on site for first time
-  if (history.length > 2) {
+  // it will be 2 if landing on site for the first time from URL in a new tab
+  if (document.referrer == '' && history.length == 2) {
+    return;
+  // it will be 1 if site was landed upon for the first time from a link somewhere else
+  } else if (history.length == 1) {
+    return;
+  // when both the above are false, show the back button
+  } else {
     backButton.classList.add('back-button-active');
   };
 
