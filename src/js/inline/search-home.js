31e29b4f738796395cdb0eec8a5e64c8f75ae911
rhoover <robin.hoover@gmail.com>
2025-06-15 11:29:37 -0400

    Better Listeners

--- src/js/inline/search-home.js
+++ src/js/inline/search-home.js
@@ -11,36 +11,48 @@
       response = await fetch('/all-artisan-data/all-artisan-data.json');
       artisanData = await response.json();
 
-      searchInput.addEventListener('focus', () => {
-        resultsList.classList.add('home-search-results-active');
-      });
-      searchInput.addEventListener('blur', () => {
-        resultsList.classList.remove('home-search-results-active');
-        searchInput.value = '';
-        resultsList.innerHTML = '';
-      });
+      ['focus', 'blur', 'keyup', 'input'].forEach(event => searchInput.addEventListener(event, doSomething));
 
-      searchInput.addEventListener('keyup', () => {
-        searchQuery = searchInput.value.toLowerCase();
-        resultsList.innerHTML = '';
+      function doSomething(e) {
+        let happening = e.type;
 
-        // https://stackoverflow.com/questions/69917128/how-to-search-a-json-file-from-a-search-bar-in-html
-        for (let i = 0; i < artisanData.length; i++) {
-          let obj = artisanData[i];
+        switch (happening) {
+          case 'focus':
+          resultsList.classList.add('home-search-results-active');
+            
+          break;
+          case 'blur':
+            resultsList.classList.remove('home-search-results-active');
+            searchInput.value = '';
+            resultsList.innerHTML = '';
+            
+          break;
+          case 'keyup':
+            searchQuery = searchInput.value.toLowerCase();
+            resultsList.innerHTML = '';
 
-          // send the results to a function to display them
-          if (obj.name.toLowerCase().includes(searchQuery) || obj.city.toLowerCase().includes(searchQuery)) {
-            displayResults(obj);
-          };
-        };
-      });
+            // https://stackoverflow.com/questions/69917128/how-to-search-a-json-file-from-a-search-bar-in-html
+            for (let i = 0; i < artisanData.length; i++) {
+              let obj = artisanData[i];
 
-      // for clearing the results when the clear button is clicked
-      searchInput.addEventListener('input', () => {
-        if (searchInput.value.length == 0) {
-        resultsList.innerHTML = '';          
-        };
-      });
+              // send the results to a function to display them
+              if (obj.name.toLowerCase().includes(searchQuery) || obj.city.toLowerCase().includes(searchQuery)) {
+                displayResults(obj);
+              };
+            };
+            
+          break;
+          case 'input':
+            if (searchInput.value.length == 0) {
+            resultsList.innerHTML = '';          
+            };
+            
+          break;
+        
+          default:
+          break;
+        }; //end switch
+      };
       
     } catch (error) {
       console.log(error);
